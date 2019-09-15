f441acdd7ff74ef95e60f46b6cc10f9790225666
Trentus <Trentus@noemails.com>
2019-09-15 00:08:14 -0400

    created item dialogs with detailed view, initialized session page

--- src/components/Items.vue
+++ src/components/Items.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container class="white--text">
         <p class="text-center display-3"><span class="font-weight-thin">Loot Of</span> The Few </p>
-        <v-card>
+        <v-card class="custom-color">
             <v-card-title>
             Collected Items
             <div class="flex-grow-1"></div>
@@ -11,32 +11,59 @@
                 label="Search"
                 single-line
                 hide-details
+                dark
             ></v-text-field>
             </v-card-title>
             <v-data-table
                 :headers="headers"
-                :items="items"
                 :items-per-page="10"
-                class="elevation-1"
+                :items="items"
                 :search="search"
                 loading-text="Please wait, table is loading...."
+                :sort-by="name"
+                :sort-desc=false
+                class="elevation-1"
+                                
             >
-                <template v-slot:item.property="{ item }">
-                    <span v-for="(prop, index) in item.property"
-                        :key="index"> {{prop}} </span>
+                <template v-slot:body="{ items }">
+                    <tbody>
+                        <tr v-for="item in items" :key="item.name" @click="setItem(item)">
+                            <td>{{item.name}}</td>
+                            <td>{{item.weaponCategory}}</td>
+                            <td>{{item.bonus}}</td>
+                            <td><span v-if="item.rarity!='Unknown'">{{item.rarity}}</span></td>
+                            <td><span>{{item.dmg1}}<span v-if="item.dmg2">/{{item.dmg2}}</span> </span></td>
+                            <td>{{item.dmgType}}</td>
+                            <td><span v-for="(prop, i) in item.property" :key="i"> {{prop}} </span></td>
+                            <td>{{item.range}}</td>
+                            <td>{{item.reqAttune}}</td>
+                            <td>{{item.owner}}</td>
+                        </tr>
+                    </tbody>
                 </template>
-
-                <template v-slot:item.dmg1="{ item }">
-                    <span>{{item.dmg1}}<span v-if="item.dmg2">/{{item.dmg2}}</span> </span>
-                </template>
-
-                <template v-slot:item.name="{ item }">
-                    <v-btn text small tile :href="item.url">{{item.name}}</v-btn>
-                </template>
-                
-            
             </v-data-table>
         </v-card>
+
+        <v-row justify="center">
+            <v-dialog v-model="itemView" max-width="600">
+            <v-card>
+                <v-card-title class="headline">{{currentItem.name}}</v-card-title>
+                <v-card-text>
+                    <p class="font-italic caption mt-n3 mb-n1">Owned By: <span v-if="currentItem.owner">{{currentItem.owner}}</span><span v-else>--</span> </p>
+                    <p class="subtitle-2 mb-n1">{{currentItem.weaponCategory}} - {{currentItem.rarity}}</p>
+                    <p class="overline">Properties: <span class="font-italic" v-for="(prop,i) in currentItem.property" :key="i">{{prop}} </span></p>
+                    <p class="mb-n1 title">Description</p>
+                    <p class="caption" v-for="(desc,i) in currentItem.description" :key="i">{{desc}}</p>   
+
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn v-if="currentItem.url" color="green darken-1" text :href="currentItem.url">5eTools Entry</v-btn>
+                    <div class="flex-grow-1"></div>
+                    <v-btn color="blue darken-1" text @click="itemView = false">Close</v-btn>
+                </v-card-actions>
+            </v-card>
+            </v-dialog>
+        </v-row>
         
     </v-container>
 </template>
@@ -46,12 +73,25 @@ import items from "@/data/items.json"
 
 export default {
     name: "Items",
+    methods: {
+        setItem: function(item) {
+            console.log('Clicked on item: ' + item.name)
+            this.resetItem
+            this.currentItem = item
+            this.itemView = true
+        },
+        resetItem: function() {
+            this.currentItem = []
+        }
+    },
     data ()  {
         return {
             search: '',
+            currentItem: [],
             items: items,
+            itemView: false,
             headers: [
-                { text: 'Item Name', align: 'left', sortable: false, value: 'name'},
+                { text: 'Item Name', align: 'left', sortable: true, value: 'name'},
                 { text: 'Category', value: 'weaponCategory'},
                 { text: 'Magic Bonus', value: 'bonus'},
                 { text: 'Rarity', value: 'rarity'},
@@ -60,8 +100,7 @@ export default {
                 { text: 'Properties', value: 'property'},
                 { text: "Range", value: "range"},
                 { text: 'Attunement', value: 'reqAttune'},
-                { text: 'Owned By', value: 'owner'},
-                
+                { text: 'Owned By', value: 'owner'}, 
             ]
         }
     }
@@ -69,5 +108,9 @@ export default {
 </script>
 
 <style scoped>
+    .custom-color{
+        background-color: #414247;
+        color: white;
+    }
 
 </style>
--- src/components/SessionNotes.vue
+++ src/components/SessionNotes.vue
@@ -1,5 +1,8 @@
 <template>
-    <div><p class="text-center">Session View</p></div>
+    <v-container class="white--text">
+        <p class="text-center display-3"><span class="font-weight-thin">Stories Of</span> The Few </p>
+        <p class="text-center caption">This section is a work in progress</p>
+    </v-container>
 </template>
 
 <script>

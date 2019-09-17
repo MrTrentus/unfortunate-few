0d0f70a017a2d2d6b77b59a524d347f0d752fcd6
Trentus <Trentus@noemails.com>
2019-09-16 21:17:14 -0400

    Started adding pictures and front-end handling

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -6,13 +6,24 @@
                     :key="index">
                     <v-card color="#414247" dark
                         :to="{ name: 'character', params: {character: character, name: character.short_name}}">
-                        <v-card-title>{{character.name}}</v-card-title>
+                        <v-card-title>
+                            {{character.name}}
+                        </v-card-title>
                         <v-card-text>
                             <p class="font-italic caption mt-n3 mb-n1">{{character.race}}</p>
                             <p class="subtitle-2 mb-n1">{{character.class}} - {{character.subclass}}</p>
                             <p class="overline">Height:{{character.height}} - Weight:{{character.weight}} - Age: {{character.age}}</p>
+                            <!--<p class="mb-n1 title">Description:</p>
+                            <p class="subtitle-1">{{character.description}}</p>-->
+                            <v-row align="center" justify="center">
+                                <v-img v-if="character.image"
+                                    :src="fetchImage(character.image)"
+                                    max-height="150"
+                                    max-width="150"
+                                ></v-img>
+                            </v-row>     
                             <p class="mb-n1 title">Description:</p>
-                            <p class="subtitle-1">{{character.description}}</p>                            
+                            <p class="subtitle-1">{{character.description}}</p>                       
                         </v-card-text>
                     </v-card>
             </v-col>
@@ -27,6 +38,12 @@ export default {
     name: "Characters",
     mounted() {
         //console.log(this.characters)
+    },
+    methods: {
+        fetchImage(img){
+            console.log("Got image: " + img)
+            return require('../assets/Party/' + img + '.png')
+        }
     },
     data: () => ({
         characters: characters,
